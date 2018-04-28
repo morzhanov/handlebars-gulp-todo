36b1052d5d19b2f60e41a03c100b5a5c24d1597c
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-04-28 10:01:11 +0300

    Created gitignore

--- handlebars/gulpfile.js
+++ handlebars/gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('css', function () {
   return gulp.src('./src/css/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 gulp.task('css:watch', function () {
   gulp.watch('./src/css/*.scss', ['css']);
