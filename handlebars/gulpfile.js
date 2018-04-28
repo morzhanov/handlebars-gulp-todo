4637d96c2d4b3b75130e527c91dc39a00122d000
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-04-28 09:59:31 +0300

    Created gulp configuration with SASS and Postcss

---
+++ handlebars/gulpfile.js
@@ -0,0 +1,17 @@
+const postcss = require('gulp-postcss');
+const gulp = require('gulp');
+const autoprefixer = require('autoprefixer');
+const sass = require('gulp-sass');
+
+gulp.task('css', function () {
+  const plugins = [
+    autoprefixer({browsers: ['last 1 version']})
+  ];
+  return gulp.src('./src/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./src/css'));
+});
+gulp.task('css:watch', function () {
+  gulp.watch('./src/css/*.scss', ['css']);
+});
