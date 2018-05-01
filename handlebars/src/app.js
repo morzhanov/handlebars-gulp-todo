46195df7f7787a39d29610f6f2c9d3a6dcb04ab4
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-05-01 21:47:01 +0300

    Created working TODO app using handlebars

--- handlebars/src/app.js
+++ handlebars/src/app.js
@@ -3,6 +3,7 @@ class Todo {
     this.id = id
     this.content = content
     this.done = done
+    this.edit = false
   }
 }
 
@@ -19,6 +20,25 @@ class State {
   removeTodo (id) {
     _.remove(this.todos, { id: id })
     renderMain()
+  }
+
+  setEditing (id) {
+    const todo = _.find(this.todos, o => o.id === id)
+      todo.edit = true
+      renderMain()
+      const editInputs = $('.edit')
+      _.each(editInputs, el => {
+        $(el).keypress(e => {
+          if (e.keyCode === 13) {
+           todo.content = el.value
+           todo.edit = false
+           renderMain()
+           _.each(state.todos, e => {
+            console.log(e.id)
+           })
+          }
+        })
+      })
   }
 
   toggleDone (id) {
@@ -53,6 +73,34 @@ const initDeleteButtons = () => {
   })
 }
 
+const initEdit = () => {
+  const views = $('.view')
+  _.each(views, el => {
+    $(el).dblclick(e => {
+      const id = +el.dataset.id
+      state.setEditing(id)
+    })
+  })
+}
+
+const hydrateTodos = () => {
+  const hydrated = JSON.stringify(state.todos)
+  localStorage.setItem('todos', hydrated)
+}
+
+const rehydrateTodos = () => {
+  const hydrated = localStorage.getItem('todos')
+  if(hydrated) {
+    const json = JSON.parse(hydrated)
+    const todos = _.map(json, e => {
+      const todo = new Todo()
+      e.edit = false
+      return Object.assign(todo, e)
+    })
+    state.todos = todos
+  }
+}
+
 const renderHeader = () => {
   const template = $('#header-template').html()
   const templateScript = Handlebars.compile(template)
@@ -62,13 +110,18 @@ const renderHeader = () => {
 const renderMain = () => {
   const template = $('#main-template').html()
   const templateScript = Handlebars.compile(template)
-  const context = { todos: state.todos }
+  const context = { 
+    todos: state.todos
+  }
   main.innerHTML = templateScript(context)
   initCheckboxes()
   initDeleteButtons()
+  initEdit()
+  hydrateTodos()
 }
 
 const init = () => {
+  rehydrateTodos()
   renderHeader()
   renderMain()
 
@@ -76,12 +129,17 @@ const init = () => {
 
   newInput.onkeypress = e => {
     if (e.keyCode === 13) {
-      console.log(e)
-      const id = state.todos.length
+      let id
+      if(!state.todos.length) {
+        id = 0
+      } else {
+        id = state.todos[state.todos.length - 1].id + Date.now()
+      }
       const content = $(newInput).val()
       const todo = new Todo(id, content)
       state.addTodo(todo)
       newInput.value = ''
+      console.log(state.todos)
     }
   }
 }
