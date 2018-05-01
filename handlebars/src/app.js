05e6672143c41ac5aa1c37cb74b2db03c6bcc456
VladMorzhanov <vlad.morzhanov@gmail.com>
2018-05-01 20:07:09 +0300

    Added handlebars code into main section

--- handlebars/src/app.js
+++ handlebars/src/app.js
@@ -1,5 +1,5 @@
 class Todo {
-  constructor (id, content, done) {
+  constructor (id, content, done = false) {
     this.id = id
     this.content = content
     this.done = done
@@ -7,28 +7,58 @@ class Todo {
 }
 
 class State {
-  constructor (){
+  constructor () {
     this.todos = []
   }
 
-  addTodo(todo) {
+  addTodo (todo) {
+    this.todos.push(todo)
+    console.log(`todos count = ${this.todos.length}`)
+    renderMain()
+  }
+
+  removeTodo (id) {
 
   }
 
-  removeTodo(id){
-
-  }
-
-  toggleDone(id) {
+  toggleDone (id) {
     return _.find(this.todos, o => o.id === id)
   }
 }
 
-const init = () => {
-  console.log('loaded')
-  const s = new State()
-  console.log(s)
-  console.log(s.todos)
+const header = $('#header')[0]
+const main = $('#main')[0]
+const state = new State()
+let newInput
+
+const renderHeader = () => {
+  const template = $('#header-template').html()
+  const templateScript = Handlebars.compile(template)
+  const context = {}
+  header.innerHTML = templateScript(context)
+}
+const renderMain = () => {
+  const template = $('#main-template').html()
+  const templateScript = Handlebars.compile(template)
+  const context = { todos: state.todos }
+  main.innerHTML = templateScript(context)
 }
 
+const init = () => {
+  renderHeader()
+  renderMain()
+
+  newInput = $('input.new-todo')[0]
+
+  newInput.onkeypress = e => {
+    if (e.keyCode === 13) {
+      console.log(e)
+      const id = state.todos.length
+      const content = $(newInput).val()
+      const todo = new Todo(id, content)
+      state.addTodo(todo)
+      newInput.value = ''
+    }
+  }
+}
 window.onload = init
